f29aaefceea150a9357e27892c0f6a8c95863cb6
Hridoy Mozumder <hrrarya6@gmail.com>
2022-10-13 17:27:32 +0600

    update for checking files concurrently

--- simple-check.sh
+++ simple-check.sh
@@ -4,7 +4,7 @@ woo_template="$path/templates/woocommerce"
 
 for singlepath in "$path/includes/modules/*/**.php"
 do
-    ./vendor/bin/phpcs --standard=ruleset.xml ${singlepath}
+    ./vendor/bin/phpcs --standard=ruleset.xml ${singlepath} &
 done
 
 
@@ -13,7 +13,7 @@ if [[ -d ${woo_template} ]]
 then
 	for template_path in "$woo_template/*/**.php"
 	do 
-		./vendor/bin/phpcs --standard=ruleset.xml ${template_path}
+		./vendor/bin/phpcs --standard=ruleset.xml ${template_path} &
 	done
 fi 
 
