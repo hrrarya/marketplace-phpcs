5c33aced4cc6231a523cd19ad11a7488b4db9d85
Hridoy Mozumder <hrrarya6@gmail.com>
2022-10-15 12:32:27 +0600

    removing concurrent mode

--- simple-check.sh
+++ simple-check.sh
@@ -2,9 +2,11 @@
 read -p 'Enter your module folder path(avoid last Oblique): ' path
 woo_template="$path/templates/woocommerce"
 
+echo "" > "$path/phpcs-log.txt";
+
 for singlepath in "$path/includes/modules/*/**.php"
 do
-    ./vendor/bin/phpcs --standard=ruleset.xml ${singlepath} &
+    ./vendor/bin/phpcs --standard=ruleset.xml ${singlepath} >> "$path/phpcs-log.txt" &
 done
 
 
@@ -13,7 +15,7 @@ if [[ -d ${woo_template} ]]
 then
 	for template_path in "$woo_template/*/**.php"
 	do 
-		./vendor/bin/phpcs --standard=ruleset.xml ${template_path} &
+		./vendor/bin/phpcs --standard=ruleset.xml ${template_path} >> "$path/phpcs-log.txt" &
 	done
 fi 
 
